0d29058a30630582df3682a4c8ae7eb500982d7a
Dennis Rippinger <dennis.rippinger@msg.group>
2018-09-03 23:48:08 +0200

    Added description for Spring Rule

--- src/main/java/de/drippinger/spring/SortOfConfiguration.java
+++ src/main/java/de/drippinger/spring/SortOfConfiguration.java
@@ -3,14 +3,13 @@ package de.drippinger.spring;
 import org.springframework.context.annotation.Bean;
 
 /**
- * Bean
+ * The JQA Spring Rule triggers here, because we use @{@link Bean} outside of a @Configuration Class.
  *
  * @author Dennis Rippinger
  */
 public class SortOfConfiguration {
 
-
-	@Bean
+    @Bean
 	public String buildConfig() {
 		return "Test";
 	}
